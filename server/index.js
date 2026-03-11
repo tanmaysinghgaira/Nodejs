8c02c549df9e9e039553c1271e1f460cc1d041c6
Tanmay gaira <tanmaygaira911@gmail.com>
2026-03-11 20:42:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,2 @@
-const http = require(`http`);
-const host = 'localhost';
-const port =700;
-
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':"txt/plain"});
-    res.end('HELLO WORLD! \n');
-});
-server.listen(port,host,()=>{
-
-console.log(`server running at http://${host}:${port}/`);
-});
+console.log('hello ');
+const math = require('./Math')
